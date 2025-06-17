9f958f39d3cfd3c9d6667d1f5e1ca4eabc5e45cb
Azeez1 <sabaazeez12@gmail.com>
2025-06-17 17:55:06 -0400

    Improve html2pdf sourcemap removal

--- frontend/scripts/remove-sourcemap.js
+++ frontend/scripts/remove-sourcemap.js
@@ -2,13 +2,21 @@ const fs = require('fs');
 const path = require('path');
 
 const distPath = path.join(__dirname, '..', 'node_modules', 'html2pdf.js', 'dist');
-const files = ['es6-promise.auto.js', 'es6-promise.auto.min.js', 'es6-promise.js', 'es6-promise.min.js'];
+const files = [
+  'es6-promise.auto.js',
+  'es6-promise.auto.min.js',
+  'es6-promise.js',
+  'es6-promise.min.js',
+  'html2pdf.js',
+  'html2pdf.bundle.js',
+  'html2pdf.bundle.min.js'
+];
 
 files.forEach((file) => {
   const fullPath = path.join(distPath, file);
   if (fs.existsSync(fullPath)) {
-    let data = fs.readFileSync(fullPath, 'utf8');
-    const updated = data.replace(/\/\/# sourceMappingURL=.*\.map\n?/, '');
+    const data = fs.readFileSync(fullPath, 'utf8');
+    const updated = data.replace(/\/\/# sourceMappingURL=.*\n?/g, '');
     if (updated !== data) {
       fs.writeFileSync(fullPath, updated, 'utf8');
       console.log(`Removed sourceMappingURL from ${file}`);
