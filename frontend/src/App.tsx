d312830e209c9ed0a51bc3c6c9ddc7ac4ae2bdec
Azeez1 <sabaazeez12@gmail.com>
2025-06-17 15:10:13 -0400

    Add React frontend scaffold with DataInputForm

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DataInputForm from './components/DataInputForm';
+
+export default function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <DataInputForm />
+    </div>
+  );
+}
